9022acb778d88870072dd8a72bbbb186b04376b7
hanny1226 <hanny1226@naver.com>
2022-07-23 01:24:54 +0900

    220723 백준

---
+++ AlgorithmStudy/src/Baekjoon/IO/operation/Baekjoon_25083.java
@@ -0,0 +1,14 @@
+package Baekjoon.IO.operation;
+
+public class Baekjoon_25083 {
+    public static void main(String[] args) {
+        System.out.println(
+                "         ,r'\"7\n"+
+                "r`-_   ,'  ,/\n"+
+                " \\. \". L_r'\n"+
+                "   `~\\/\n"+
+                "      |\n"+
+                "      |"
+                );
+    }
+}
