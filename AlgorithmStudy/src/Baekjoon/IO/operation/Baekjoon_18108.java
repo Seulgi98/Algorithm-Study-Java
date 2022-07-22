51540a15f40bf135513128c76e1e0b00cc84ab46
hanny1226 <hanny1226@naver.com>
2022-07-23 00:47:55 +0900

    220723 백준

---
+++ AlgorithmStudy/src/Baekjoon/IO/operation/Baekjoon_18108.java
@@ -0,0 +1,18 @@
+package Baekjoon.IO.operation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baekjoon_18108 {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int year = Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(year-543));
+        bw.flush();
+        bw.close();
+    }
+}
