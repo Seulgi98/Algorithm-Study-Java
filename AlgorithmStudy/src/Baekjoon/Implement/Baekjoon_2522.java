4fa12d3e532dbe6dca59cc35ccb53f8a3763222f
hanny1226 <hanny1226@naver.com>
2022-07-24 02:43:33 +0900

    220724 백준

---
+++ AlgorithmStudy/src/Baekjoon/Implement/Baekjoon_2522.java
@@ -0,0 +1,31 @@
+package Baekjoon.Implement;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon_2522 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=1; i<=N; i++) {
+            for (int j=0; j<N-i; j++){
+                System.out.print(" ");
+            }
+            for (int j=0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i=1; i<N; i++) {
+            for (int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+            for (int j=0; j<N-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
