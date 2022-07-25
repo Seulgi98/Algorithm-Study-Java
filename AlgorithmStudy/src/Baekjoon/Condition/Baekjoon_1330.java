d018d61f0f8ff165bdd463c3543ff1f0fd70ff26
hanny1226 <hanny1226@naver.com>
2022-07-25 22:37:48 +0900

    220725 백준

--- AlgorithmStudy/src/Baekjoon/Condition/Baekjoon_1330.java
+++ AlgorithmStudy/src/Baekjoon/Condition/Baekjoon_1330.java
@@ -1,9 +1,7 @@
-package Baekjoon.condition;
+package Baekjoon.Condition;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Baekjoon_1330 {
     public static void main(String[] args) throws Exception {
