de4d49262084042dd460ec2acc35bf68c24342aa
hanny1226 <hanny1226@naver.com>
2022-07-25 22:37:12 +0900

    220725 백준

---
+++ AlgorithmStudy/src/Baekjoon/condition/Baekjoon_1330.java
@@ -0,0 +1,26 @@
+package Baekjoon.condition;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baekjoon_1330 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] line = br.readLine().split(" ");
+        int a = Integer.parseInt(line[0]);
+        int b = Integer.parseInt(line[1]);
+
+        if(a>b){
+            System.out.print(">");
+        }
+        if(a<b){
+            System.out.print("<");
+        }
+        else if(a==b) {
+            System.out.print("==");
+        }
+    }
+}
