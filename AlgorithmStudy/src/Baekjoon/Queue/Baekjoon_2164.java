f131b5387ace390fce746a1a0e494bcfcb2c8caf
seulgi <hanny1226@naver.com>
2022-08-04 19:18:27 +0900

    220804 백준

---
+++ AlgorithmStudy/src/Baekjoon/Queue/Baekjoon_2164.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> list = new ArrayDeque<>();
+
+        for(int i=1; i<=N; i++){
+            list.addLast(i);
+        }
+
+        while (list.size() != 1){
+            list.poll();
+            list.offerLast(list.poll());
+        }
+        System.out.println(list.poll());
+    }
+}
