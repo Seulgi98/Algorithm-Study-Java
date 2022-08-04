1d93cf30bfa5ab76c1527e2f87d7ef54000766c6
seulgi <hanny1226@naver.com>
2022-08-04 19:23:31 +0900

    220802 SWEA

---
+++ AlgorithmStudy/src/SWEA/D2/SWEA_2001.java
@@ -0,0 +1,41 @@
+package hwalgo03_부울경_04반_박슬기;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=T; tc++) {
+			String[] line1 = br.readLine().split(" ");
+			int N = Integer.parseInt(line1[0]);
+			int M = Integer.parseInt(line1[1]);
+			
+			int[][] map = new int[N][N];
+			
+			for(int i=0; i<N; i++) {
+				String[] str = br.readLine().split(" ");
+				for(int j=0; j<N; j++) {
+					map[i][j] = Integer.parseInt(str[j]);
+				}
+			}
+			int sum=0;
+			int max = 0;
+			
+			for(int i=0; i<=N-M; i++) {
+				for(int j=0; j<=N-M; j++) {
+					for(int k=0; k<M; k++) {
+						for(int l=0; l<M; l++) {
+							sum += map[i+k][j+l];
+						}
+					}
+					max = Math.max(max, sum);
+					sum = 0;
+				}
+			}
+			System.out.printf("#%d %d\n", tc, max);
+		}
+	}
+}
