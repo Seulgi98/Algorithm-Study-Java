d1536b36304c4affe12abf45cdfddda34b43baf0
seulgi <hanny1226@naver.com>
2022-08-17 00:33:12 +0900

    220817 백준

--- AlgorithmStudy/src/Baekjoon/BFS/Baekjoon_1697.java
+++ AlgorithmStudy/src/Baekjoon/BFS/Baekjoon_1697.java
@@ -2,18 +2,15 @@ package Baekjoon.BFS;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Baekjoon_1697 {
     static int n, k;
-    static int[] arr;
+    static int[] arr = new int[100001];
     static Queue<Integer> q = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
@@ -21,7 +18,6 @@ public class Baekjoon_1697 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-
 
         if (n >= k) {
             System.out.println(n - k);
@@ -32,28 +28,26 @@ public class Baekjoon_1697 {
     }
 
     static int bfs() {
-
-        // 시간을 저장하는 배열선언
-        arr = new int[100001];
         q.add(n);
         arr[n] = 1;
+
         while (!q.isEmpty()) {
             int x = q.poll();
             for (int i = 0; i < 3; i++) {
-                int nx;
+                int next;
                 if (i == 0)
-                    nx = x - 1;
+                    next = x - 1;
                 else if (i == 1)
-                    nx = x + 1;
+                    next = x + 1;
                 else
-                    nx = x * 2;
+                    next = x * 2;
 
-                if (nx == k)
+                if (next == k)
                     return arr[x];
 
-                if (nx >= 0 && nx < 100001 && arr[nx] == 0) {
-                    arr[nx] = arr[x] + 1;
-                    q.add(nx);
+                if (next >= 0 && next < 100001 && arr[next] == 0) {
+                    arr[next] = arr[x] + 1;
+                    q.add(next);
                 }
             }
         }
