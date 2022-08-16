cb551bdff1d7fc876265109808845bb2977e477e
seulgi <hanny1226@naver.com>
2022-08-16 23:56:14 +0900

    220816 백준

--- AlgorithmStudy/src/Baekjoon/BFS/Baekjoon_24444.java
+++ AlgorithmStudy/src/Baekjoon/BFS/Baekjoon_24444.java
@@ -1,63 +1,63 @@
 package Baekjoon.BFS;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Baekjoon_24444 {
-    static int N, M;
-    static List<List<Integer>> l = new ArrayList<>();
-    static int[] visited;
+    static StringTokenizer st;
+    static int N, M, R;
+    static List<Integer> []list;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st=new StringTokenizer(br.readLine());
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        R=Integer.parseInt(st.nextToken());
+        list=new ArrayList[N+1];
+        for(int i=1;i<=N;i++) list[i]=new ArrayList<>();
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        int R = Integer.parseInt(st.nextToken());
-
-        visited = new int[N+1];
-        for(int i = 0; i<=N; i++)
-            l.add(new ArrayList<>());
-
-        for(int i = 0; i<M; i++){
-            st = new StringTokenizer(br.readLine());
-
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-
-            l.get(A).add(B);
-            l.get(B).add(A);
+        for(int i=0;i<M;i++) {
+            st=new StringTokenizer(br.readLine());
+            int u=Integer.parseInt(st.nextToken());
+            int v=Integer.parseInt(st.nextToken());
+            list[u].add(v);
+            list[v].add(u);
         }
-
-        for(int i = 1; i<=N; i++)
-            Collections.sort(l.get(i));
-
+        for(int i=1;i<=N;i++) Collections.sort(list[i], Collections.reverseOrder());
         bfs(R);
-
-        for(int i = 1; i<=N; i++)
-            System.out.println(visited[i]);
     }
 
-    static void bfs(int start){
-        Queue<Integer> q = new LinkedList<>();
-        int cnt = 1;
+    private static void bfs(int x) {
+        Queue<Integer> queue=new LinkedList<>();
+        queue.add(x);
 
-        q.offer(start);
-        visited[start] = cnt++;
+        boolean []visited=new boolean[N+1];
+        visited[x]=true;
 
-        while(!q.isEmpty()){
-            int a = q.poll();
+        int cnt=0;
+        int []order=new int[N+1];
+        while(!queue.isEmpty()) {
+            int q=queue.poll();
+            cnt++;
+            order[q]=cnt;
 
-            for(int i = 0; i<l.get(a).size(); i++){
-                int nextV = l.get(a).get(i);
-
-                if(visited[nextV] != 0)
-                    continue;
-
-                q.offer(nextV);
-                visited[nextV] = cnt++;
+            for(int i:list[q]) {
+                if(!visited[i]) {
+                    visited[i]=true;
+                    queue.add(i);
+                }
             }
         }
+
+        for(int i=1;i<=N;i++) System.out.println(order[i]);
     }
+
 }
